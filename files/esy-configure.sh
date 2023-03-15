fd3f8dd29ff8d896e7a1a7cade84e83642835876
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2023-03-15 12:20:22 +0530

    package gmp 6.2.1

---
+++ files/esy-configure.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+CONFIGURE_FLAGS=("--prefix=$cur__install" "--enable-cxx" "--with-pic")
+
+UNAME_M=$(uname -m)
+
+ARCH="${UNAME_M%%*( )}"
+OS=$(uname -s)
+
+if [ "$OS" == "Darwin" ]
+then
+    CONFIGURE_FLAGS+=("--build=${ARCH}-apple-darwin$(uname -r)")
+fi
+
+case "$(uname -s)" in
+    CYGWIN*|MINGW32*|MSYS*)
+       CONFIGURE_FLAGS+=("--host x86_64-w64-mingw32")
+     ;;
+    *)
+	true
+        ;;
+esac
+
+CONFIGURE_ARGS=$(printf '%s ' "${CONFIGURE_FLAGS[@]}")
+./configure $CONFIGURE_ARGS
