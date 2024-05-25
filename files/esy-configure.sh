ad4bbb07c83413f9340ad406f57a33fb18a80139
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2024-05-25 20:34:58 +0530

    Macos: Remove --build and let gmp figure it out

--- files/esy-configure.sh
+++ files/esy-configure.sh
@@ -2,16 +2,6 @@
 
 CONFIGURE_FLAGS=("--prefix=$cur__install" "--enable-cxx" "--with-pic")
 
-UNAME_M=$(uname -m)
-
-ARCH="${UNAME_M%%*( )}"
-OS=$(uname -s)
-
-if [ "$OS" == "Darwin" ]
-then
-    CONFIGURE_FLAGS+=("--build=${ARCH}-apple-darwin$(uname -r)")
-fi
-
 case "$(uname -s)" in
     CYGWIN*|MINGW32*|MSYS*)
        CONFIGURE_FLAGS+=("--host x86_64-w64-mingw32")
